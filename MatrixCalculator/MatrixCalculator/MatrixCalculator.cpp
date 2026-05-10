c5de2894fc04653a9902b61ea90c9e359626a404
ryazanovnikitaeaa-source <ryazanovnikitaeaa@gmail.com>
2026-05-10 23:26:51 +0300

    Реализация оператора сложения и обработка исключений

--- MatrixCalculator/MatrixCalculator/MatrixCalculator.cpp
+++ MatrixCalculator/MatrixCalculator/MatrixCalculator.cpp
@@ -17,7 +17,7 @@ public:
         values.assign(r, vector<double>(c, 0.0));
     }
 
-    // Заполнениез
+    // Заполнение
     void fillFromConsole() {
         cout << "Введите числа для таблицы " << r << "x" << c << ":" << endl;
         for (int i = 0; i < r; i++) {
@@ -38,20 +38,45 @@ public:
             cout << endl;
         }
     }
+    // Сложение матриц
+    MyMatrix operator+(const MyMatrix& other) {
+        if (r != other.r || c != other.c) {
+            throw invalid_argument("Размеры матриц не совпадают, сложение невозможно!");
+        }
+
+        MyMatrix result(r, c);
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                result.values[i][j] = this->values[i][j] + other.values[i][j];
+            }
+        }
+        return result;
+    }
 };
 
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    int rows, cols;
-    cout << "Введите количество строк: ";
-    cin >> rows;
-    cout << "Введите количество столбцов: ";
-    cin >> cols;
+    try {
+        int rows, cols;
+        cout << "Введите размер матриц: ";
+        cin >> rows >> cols;
 
-    MyMatrix m(rows, cols);
-    m.fillFromConsole();
-    m.show();
+        cout << "\n--- Заполнение первой матрицы ---" << endl;
+        MyMatrix m1(rows, cols);
+        m1.fillFromConsole();
+
+        cout << "\n--- Заполнение второй матрицы ---" << endl;
+        MyMatrix m2(rows, cols);
+        m2.fillFromConsole();
+
+        cout << "\nСкладываем матрицы..." << endl;
+        MyMatrix sum = m1 + m2;
+        sum.show();
+    }
+    catch (const exception& e) {
+        cout << "Ошибка: " << e.what() << endl;
+    }
 
     return 0;
 }
